2f00205844e717d1f34077a8f0f462171e74dfa0
Dante <guchoo923@gmail.com>
2022-05-17 22:55:38 +0300

    Add full functionality to carousel

--- script.js
+++ script.js
@@ -2,19 +2,36 @@ const slider = document.querySelector('.slider');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right')
 const articles = Array.from(document.querySelectorAll('.slider-section'));
+const controlIndicators = Array.from(document.querySelectorAll('.controls-ul li'));
+const indicatorList = document.querySelector('.controls-ul')
 let sectionIndex = 0;
-console.log(articles)
+
+const moveSlider = () => {
+    slider.style.transform = `translate(-${sectionIndex*25}%)`;
+    document.querySelector('.selected').classList.remove('selected');
+}
 
 
-rightArrow.addEventListener('click',() => {
+controlIndicators.forEach( (indicator,index) => {
 
+    indicator.addEventListener( 'click', () => {
+        sectionIndex = index;
+        moveSlider();
+        indicator.classList.add('selected');
+
+    })
+})
+
+rightArrow.addEventListener( 'click',() => {
     sectionIndex = (sectionIndex<articles.length-1) ? sectionIndex+1 : 0;
-    slider.style.transform = `translate(-${sectionIndex*25}%)`
+    moveSlider();
+    indicatorList.children[sectionIndex].classList.add('selected')
 
 })
-leftArrow.addEventListener('click', () => {
 
+leftArrow.addEventListener( 'click', () => {
     sectionIndex = (sectionIndex>0) ? sectionIndex-1 : 3;
-    slider.style.transform = `translate(-${sectionIndex*25}%)`
+    moveSlider();
+    indicatorList.children[sectionIndex].classList.add('selected')
 
 })
