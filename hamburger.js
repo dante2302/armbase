2a12a552b7a02d067f98e5cccb245b8cf6e6205c
Dante <guchoo923@gmail.com>
2022-05-18 10:20:51 +0300

    Finish styling nav section

--- hamburger.js
+++ hamburger.js
@@ -3,14 +3,14 @@ const navUl = document.querySelector('nav>ul');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    navUl.classList.toggle('active');
     document.querySelector('body').classList.toggle('scrollStop')  
+    navUl.classList.toggle('active');
 })
 
 document.querySelectorAll('nav>ul>*').forEach((link)=>{
     link.addEventListener('click',()=>{
         hamburger.classList.remove('active');
-        navUl.classList.remove('active');
         document.querySelector('body').classList.remove('scrollStop')  
+        navUl.classList.remove('active');
     })
 })
